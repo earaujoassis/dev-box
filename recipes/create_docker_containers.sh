75afc744ede67e63eb8fbb634fafa227aac1e072
Ewerton Assis <earaujoassis@gmail.com>
2015-10-07 09:24:34 -0300

    Update recipe for the docker containers

--- recipes/create_docker_containers.sh
+++ recipes/create_docker_containers.sh
@@ -4,3 +4,5 @@ echo "\033[92m# Creating redis-store \033[0m"
 docker run --name redis-store -d -p 6379:6379 redis
 echo "\033[92m# Creating postgres-store \033[0m"
 docker run --name postgres-store -d -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=$DATABASE_PASSWORD -p 5432:5432 postgres
+echo "\033[92m# Creating mongo-store \033[0m"
+docker run --name mongo-store -d -p 27017:27017 mongo
