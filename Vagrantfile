c1641eb1c1ae2681f406d97c37bd540f2fb8eb25
Ewerton Assis <earaujoassis@gmail.com>
2016-05-17 19:00:20 -0300

    Update provision script

--- Vagrantfile
+++ Vagrantfile
@@ -15,6 +15,13 @@ Vagrant.configure(2) do |config|
   config.vm.network :forwarded_port, guest: 5432,  host: 5432   # Postgres
   config.vm.network :forwarded_port, guest: 5672,  host: 5672   # RabbitMQ
 
+  # Applications binds
+
+  config.vm.network :forwarded_port, guest: 33507, host: 33507
+  config.vm.network :forwarded_port, guest: 5000,  host: 5000
+  config.vm.network :forwarded_port, guest: 7654,  host: 7654
+  config.vm.network :forwarded_port, guest: 8000,  host: 8000
+
   config.vm.provider :virtualbox do |vb|
     vb.customize [
       "modifyvm", :id,
--- cli/provision.sh
+++ cli/provision.sh
@@ -2,7 +2,32 @@
 
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y build-essential git-core curl htop zsh python-dev
-echo 'LC_ALL="en_US.UTF-8"' | sudo tee /etc/environment
+sudo touch /etc/environment
+touch $HOME/.profile
+echo 'LC_ALL="en_US.UTF-8"' | sudo tee -a /etc/environment
+
+sudo apt-get install -y build-essential git-core zsh curl htop python-dev libevent-dev libffi-dev php5-cli php5-curl
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | sh
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get update
+sudo apt-get install -y --force-yes mongodb-org
+sudo service mongod start
+
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+pip install --user -I pip setuptools virtualenv virtualenvwrapper
+rm -f get-pip.py
+grep -q -F 'dsmgr-localbin' $HOME/.profile || echo 'export PATH="$HOME/bin:$HOME/.local/bin:$PATH" # dsmgr-localbin' >> $HOME/.profile
+grep -q -F 'dsmgr-venvwrapper' $HOME/.profile || echo '. $HOME/.local/bin/virtualenvwrapper.sh # dsmgr-venvwrapper' >> $HOME/.profile
+source /home/vagrant/.zshrc
+
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y --force-yes postgresql postgresql-contrib
 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --ruby
@@ -11,13 +36,22 @@ gem install bundler
 sudo usermod -aG vboxsf vagrant
 curl -sSL https://get.docker.com/ | sh
 sudo usermod -aG docker vagrant
-
-sudo apt-get autoremove -y
-sudo apt-get autoclean -y
+source /home/vagrant/.zshrc
 
 curl -OL https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh
 bash install.sh
 rm -f install.sh
+
+mkdir -p $HOME/.local
+cd $HOME
+wget https://www.python.org/ftp/python/2.7.8/Python-2.7.8.tar.xz
+tar -xf $HOME/Python-2.7.8.tar.xz
+cd $HOME/Python-2.7.8 && ./configure --prefix=$HOME/.local && make && make install
+cd $HOME && rm -rf $HOME/Python-2.7.8
+rm -f $HOME/Python-2.7.8.tar.xz
+
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
 
 echo ''
 echo 'SYSTEM IS ABOUT TO SHUTDOWN'
