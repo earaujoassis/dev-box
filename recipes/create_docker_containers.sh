400a25ae90d46dd68be2fa15a12998aa3d558f9f
Ewerton Assis <earaujoassis@gmail.com>
2016-05-22 17:08:07 -0300

    Update RabbitMQ recipe (docker container creation)

--- recipes/create_docker_containers.sh
+++ recipes/create_docker_containers.sh
@@ -7,4 +7,4 @@ docker run --name postgres-store -d -e POSTGRES_USER=postgres -e POSTGRES_PASSWO
 echo "\033[92m# Creating MongoDB container \033[0m"
 docker run --name mongo-store -d -p 27017:27017 mongo
 echo "\033[92m# Creating RabbitMQ container \033[0m"
-docker run --name rabbit-store --hostname rabbitmq-default -d -p 5672:5672 rabbitmq
+docker run --name rabbit-store --hostname rabbitmq-default -d -e RABBITMQ_DEFAULT_USER=rabbit -e RABBITMQ_DEFAULT_PASS=$RABBITMQ_PASSWORD -p 5672:5672 rabbitmq
